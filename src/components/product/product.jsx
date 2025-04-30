399e169640f70179c6095d5c9f8a51ddfa6f7606
Harshit <harshitmehta155@gmail.com>
2025-04-30 19:02:18 +0530

    Added redux store and implement api for get products

--- src/components/product/product.jsx
+++ src/components/product/product.jsx
@@ -1,249 +1,281 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../redux/cartslice";
 
 const Product = () => {
-    return(
-        <>
-        <section class="active-product-area section_gap">
-		<div class="single-product-slider">
-			<div class="container">
-				<div class="row justify-content-center">
-					<div class="col-lg-6 text-center">
-						<div class="section-title">
-							<h1>Latest Products</h1>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-								dolore
-								magna aliqua.</p>
+	const [products, setProducts] = useState([]);
+	const dispatch = useDispatch();
+
+	const handleAddToCart = () => {
+		dispatch(addToCart(products));
+	};
+
+	useEffect(() => {
+		axios.get(" http://localhost:5000/api/getproduct")
+			.then(res => {
+				setProducts(res.data.product); // Update this line if your API wraps products inside a key
+			})
+			.catch(err => {
+				console.error("Failed to fetch products:", err);
+			});
+	}, []);
+	return (
+		<>
+			<section class="active-product-area section_gap">
+				<div class="single-product-slider">
+					<div class="container">
+						<div class="row justify-content-center">
+							<div class="col-lg-6 text-center">
+								<div class="section-title">
+									<h1>Latest Products</h1>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+										dolore
+										magna aliqua.</p>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							{products?.map((product) => {
+								return (
+									<>
+										<div class="col-lg-3 col-md-6">
+											<div class="single-product">
+												<img class="img-fluid" src={`http://localhost:5000/uploads/${product.image}`} alt="" />
+												<div class="product-details">
+													<h6>{product.brand}</h6>
+													<div class="price">
+														<h6>${product.price}</h6>
+														{/* {product.originalPrice && (
+														<h6 className="l-through">${product.originalPrice}</h6>
+													)} */}
+													</div>
+													<div class="d-flex justify-content-between mt-2">
+														<span><strong>Size:</strong> {product.size}</span>
+														<span><strong>Color:</strong> {product.color}</span>
+													</div>
+													<div class="prd-bottom">
+
+														<a href="" class="social-info" onClick={handleAddToCart}>
+															<i class="ti-bag fa-solid fa-bag-shopping"></i>
+															<p class="hover-text">add to bag</p>
+														</a>
+														<a href="" class="social-info">
+															<i class="fa-regular fa-heart"></i>
+															<p class="hover-text">Wishlist</p>
+														</a>
+														<a href="" class="social-info">
+															<i class="fa-solid fa-up-down-left-right"></i>
+															<p class="hover-text">view more</p>
+														</a>
+													</div>
+												</div>
+
+											</div>
+										</div>
+									</>
+								)
+
+							})}
+							{/* <div class="col-lg-3 col-md-6">
+								<div class="single-product">
+									<img class="img-fluid" src="img/product/p2.jpg" alt="" />
+									<div class="product-details">
+										<h6>addidas New Hammer sole
+											for Sports person</h6>
+										<div class="price">
+											<h6>$150.00</h6>
+											<h6 class="l-through">$210.00</h6>
+										</div>
+										<div class="prd-bottom">
+
+											<a href="" class="social-info">
+												<i class="ti-bag fa-solid fa-bag-shopping"></i>
+												<p class="hover-text">add to bag</p>
+											</a>
+											<a href="" class="social-info">
+												<i class="fa-regular fa-heart"></i>
+												<p class="hover-text">Wishlist</p>
+											</a>
+											<a href="" class="social-info">
+												<i class="fa-solid fa-up-down-left-right"></i>
+												<p class="hover-text">view more</p>
+											</a>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-3 col-md-6">
+								<div class="single-product">
+									<img class="img-fluid" src="img/product/p3.jpg" alt="" />
+									<div class="product-details">
+										<h6>addidas New Hammer sole
+											for Sports person</h6>
+										<div class="price">
+											<h6>$150.00</h6>
+											<h6 class="l-through">$210.00</h6>
+										</div>
+										<div class="prd-bottom">
+											<a href="" class="social-info">
+												<i class="ti-bag fa-solid fa-bag-shopping"></i>
+												<p class="hover-text">add to bag</p>
+											</a>
+											<a href="" class="social-info">
+												<i class="fa-regular fa-heart"></i>
+												<p class="hover-text">Wishlist</p>
+											</a>
+											<a href="" class="social-info">
+												<i class="fa-solid fa-up-down-left-right"></i>
+												<p class="hover-text">view more</p>
+											</a>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-3 col-md-6">
+								<div class="single-product">
+									<img class="img-fluid" src="img/product/p4.jpg" alt="" />
+									<div class="product-details">
+										<h6>addidas New Hammer sole
+											for Sports person</h6>
+										<div class="price">
+											<h6>$150.00</h6>
+											<h6 class="l-through">$210.00</h6>
+										</div>
+										<div class="prd-bottom">
+
+											<a href="" class="social-info">
+												<i class="ti-bag fa-solid fa-bag-shopping"></i>
+												<p class="hover-text">add to bag</p>
+											</a>
+											<a href="" class="social-info">
+												<i class="fa-regular fa-heart"></i>
+												<p class="hover-text">Wishlist</p>
+											</a>
+											<a href="" class="social-info">
+												<i class="fa-solid fa-up-down-left-right"></i>
+												<p class="hover-text">view more</p>
+											</a>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-3 col-md-6">
+								<div class="single-product">
+									<img class="img-fluid" src="img/product/p5.jpg" alt="" />
+									<div class="product-details">
+										<h6>addidas New Hammer sole
+											for Sports person</h6>
+										<div class="price">
+											<h6>$150.00</h6>
+											<h6 class="l-through">$210.00</h6>
+										</div>
+										<div class="prd-bottom">
+
+											<a href="" class="social-info">
+												<i class="ti-bag fa-solid fa-bag-shopping"></i>
+												<p class="hover-text">add to bag</p>
+											</a>
+											<a href="" class="social-info">
+												<i class="fa-regular fa-heart"></i>
+												<p class="hover-text">Wishlist</p>
+											</a>
+											<a href="" class="social-info">
+												<i class="fa-solid fa-up-down-left-right"></i>
+												<p class="hover-text">view more</p>
+											</a>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-3 col-md-6">
+								<div class="single-product">
+									<img class="img-fluid" src="img/product/p6.jpg" alt="" />
+									<div class="product-details">
+										<h6>addidas New Hammer sole
+											for Sports person</h6>
+										<div class="price">
+											<h6>$150.00</h6>
+											<h6 class="l-through">$210.00</h6>
+										</div>
+										<div class="prd-bottom">
+
+											<a href="" class="social-info">
+												<i class="ti-bag fa-solid fa-bag-shopping"></i>
+												<p class="hover-text">add to bag</p>
+											</a>
+											<a href="" class="social-info">
+												<i class="fa-regular fa-heart"></i>
+												<p class="hover-text">Wishlist</p>
+											</a>
+											<a href="" class="social-info">
+												<i class="fa-solid fa-up-down-left-right"></i>
+												<p class="hover-text">view more</p>
+											</a>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-3 col-md-6">
+								<div class="single-product">
+									<img class="img-fluid" src="img/product/p7.jpg" alt="" />
+									<div class="product-details">
+										<h6>addidas New Hammer sole
+											for Sports person</h6>
+										<div class="price">
+											<h6>$150.00</h6>
+											<h6 class="l-through">$210.00</h6>
+										</div>
+										<div class="prd-bottom">
+
+											<a href="" class="social-info">
+												<i class="ti-bag fa-solid fa-bag-shopping"></i>
+												<p class="hover-text">add to bag</p>
+											</a>
+											<a href="" class="social-info">
+												<i class="fa-regular fa-heart"></i>
+												<p class="hover-text">Wishlist</p>
+											</a>
+											<a href="" class="social-info">
+												<i class="fa-solid fa-up-down-left-right"></i>
+												<p class="hover-text">view more</p>
+											</a>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-3 col-md-6">
+								<div class="single-product">
+									<img class="img-fluid" src="img/product/p8.jpg" alt="" />
+									<div class="product-details">
+										<h6>addidas New Hammer sole
+											for Sports person</h6>
+										<div class="price">
+											<h6>$150.00</h6>
+											<h6 class="l-through">$210.00</h6>
+										</div>
+										<div class="prd-bottom">
+
+											<a href="" class="social-info">
+												<i class="ti-bag fa-solid fa-bag-shopping"></i>
+												<p class="hover-text">add to bag</p>
+											</a>
+											<a href="" class="social-info">
+												<i class="fa-regular fa-heart"></i>
+												<p class="hover-text">Wishlist</p>
+											</a>
+											<a href="" class="social-info">
+												<i class="fa-solid fa-up-down-left-right"></i>
+												<p class="hover-text">view more</p>
+											</a>
+										</div>
+									</div>
+								</div>
+							</div> */}
 						</div>
 					</div>
 				</div>
-				<div class="row">
-					<div class="col-lg-3 col-md-6">
-						<div class="single-product">
-							<img class="img-fluid" src="img/product/p1.jpg" alt=""/>
-							<div class="product-details">
-								<h6>addidas New Hammer sole
-									for Sports person</h6>
-								<div class="price">
-									<h6>$150.00</h6>
-									<h6 class="l-through">$210.00</h6>
-								</div>
-								<div class="prd-bottom">
-
-									<a href="" class="social-info">
-                                    <i class="ti-bag fa-solid fa-bag-shopping"></i>
-										<p class="hover-text">add to bag</p>
-									</a>
-									<a href="" class="social-info">
-										<span class="lnr lnr-heart"></span>
-										<p class="hover-text">Wishlist</p>
-									</a>
-									<a href="" class="social-info">
-										<i class="fa-solid fa-up-down-left-right"></i>
-										<p class="hover-text">view more</p>
-									</a>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-3 col-md-6">
-						<div class="single-product">
-							<img class="img-fluid" src="img/product/p2.jpg" alt=""/>
-							<div class="product-details">
-								<h6>addidas New Hammer sole
-									for Sports person</h6>
-								<div class="price">
-									<h6>$150.00</h6>
-									<h6 class="l-through">$210.00</h6>
-								</div>
-								<div class="prd-bottom">
-
-									<a href="" class="social-info">
-										<i class="ti-bag fa-solid fa-bag-shopping"></i>
-										<p class="hover-text">add to bag</p>
-									</a>
-									<a href="" class="social-info">
-										<i class="fa-regular fa-heart"></i>
-										<p class="hover-text">Wishlist</p>
-									</a>
-									<a href="" class="social-info">
-										<i class="fa-solid fa-up-down-left-right"></i>
-										<p class="hover-text">view more</p>
-									</a>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-3 col-md-6">
-						<div class="single-product">
-							<img class="img-fluid" src="img/product/p3.jpg" alt=""/>
-							<div class="product-details">
-								<h6>addidas New Hammer sole
-									for Sports person</h6>
-								<div class="price">
-									<h6>$150.00</h6>
-									<h6 class="l-through">$210.00</h6>
-								</div>
-								<div class="prd-bottom">
-									<a href="" class="social-info">
-										<i class="ti-bag fa-solid fa-bag-shopping"></i>
-										<p class="hover-text">add to bag</p>
-									</a>
-									<a href="" class="social-info">
-										<i class="fa-regular fa-heart"></i>
-										<p class="hover-text">Wishlist</p>
-									</a>
-									<a href="" class="social-info">
-										<i class="fa-solid fa-up-down-left-right"></i>
-										<p class="hover-text">view more</p>
-									</a>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-3 col-md-6">
-						<div class="single-product">
-							<img class="img-fluid" src="img/product/p4.jpg" alt=""/>
-							<div class="product-details">
-								<h6>addidas New Hammer sole
-									for Sports person</h6>
-								<div class="price">
-									<h6>$150.00</h6>
-									<h6 class="l-through">$210.00</h6>
-								</div>
-								<div class="prd-bottom">
-
-									<a href="" class="social-info">
-										<i class="ti-bag fa-solid fa-bag-shopping"></i>
-										<p class="hover-text">add to bag</p>
-									</a>
-									<a href="" class="social-info">
-										<i class="fa-regular fa-heart"></i>
-										<p class="hover-text">Wishlist</p>
-									</a>
-									<a href="" class="social-info">
-										<i class="fa-solid fa-up-down-left-right"></i>
-										<p class="hover-text">view more</p>
-									</a>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-3 col-md-6">
-						<div class="single-product">
-							<img class="img-fluid" src="img/product/p5.jpg" alt=""/>
-							<div class="product-details">
-								<h6>addidas New Hammer sole
-									for Sports person</h6>
-								<div class="price">
-									<h6>$150.00</h6>
-									<h6 class="l-through">$210.00</h6>
-								</div>
-								<div class="prd-bottom">
-
-									<a href="" class="social-info">
-										<i class="ti-bag fa-solid fa-bag-shopping"></i>
-										<p class="hover-text">add to bag</p>
-									</a>
-									<a href="" class="social-info">
-										<i class="fa-regular fa-heart"></i>
-										<p class="hover-text">Wishlist</p>
-									</a>
-									<a href="" class="social-info">
-										<i class="fa-solid fa-up-down-left-right"></i>
-										<p class="hover-text">view more</p>
-									</a>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-3 col-md-6">
-						<div class="single-product">
-							<img class="img-fluid" src="img/product/p6.jpg" alt=""/>
-							<div class="product-details">
-								<h6>addidas New Hammer sole
-									for Sports person</h6>
-								<div class="price">
-									<h6>$150.00</h6>
-									<h6 class="l-through">$210.00</h6>
-								</div>
-								<div class="prd-bottom">
-
-									<a href="" class="social-info">
-										<i class="ti-bag fa-solid fa-bag-shopping"></i>
-										<p class="hover-text">add to bag</p>
-									</a>
-									<a href="" class="social-info">
-										<i class="fa-regular fa-heart"></i>
-										<p class="hover-text">Wishlist</p>
-									</a>
-									<a href="" class="social-info">
-										<i class="fa-solid fa-up-down-left-right"></i>
-										<p class="hover-text">view more</p>
-									</a>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-3 col-md-6">
-						<div class="single-product">
-							<img class="img-fluid" src="img/product/p7.jpg" alt=""/>
-							<div class="product-details">
-								<h6>addidas New Hammer sole
-									for Sports person</h6>
-								<div class="price">
-									<h6>$150.00</h6>
-									<h6 class="l-through">$210.00</h6>
-								</div>
-								<div class="prd-bottom">
-
-									<a href="" class="social-info">
-										<i class="ti-bag fa-solid fa-bag-shopping"></i>
-										<p class="hover-text">add to bag</p>
-									</a>
-									<a href="" class="social-info">
-										<i class="fa-regular fa-heart"></i>
-										<p class="hover-text">Wishlist</p>
-									</a>
-									<a href="" class="social-info">
-										<i class="fa-solid fa-up-down-left-right"></i>
-										<p class="hover-text">view more</p>
-									</a>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-3 col-md-6">
-						<div class="single-product">
-							<img class="img-fluid" src="img/product/p8.jpg" alt=""/>
-							<div class="product-details">
-								<h6>addidas New Hammer sole
-									for Sports person</h6>
-								<div class="price">
-									<h6>$150.00</h6>
-									<h6 class="l-through">$210.00</h6>
-								</div>
-								<div class="prd-bottom">
-
-									<a href="" class="social-info">
-										<i class="ti-bag fa-solid fa-bag-shopping"></i>
-										<p class="hover-text">add to bag</p>
-									</a>
-									<a href="" class="social-info">
-										<i class="fa-regular fa-heart"></i>
-										<p class="hover-text">Wishlist</p>
-									</a>
-									<a href="" class="social-info">
-										<i class="fa-solid fa-up-down-left-right"></i>
-										<p class="hover-text">view more</p>
-									</a>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		{/* <div class="single-product-slider">
+				{/* <div class="single-product-slider">
 			<div class="container">
 				<div class="row justify-content-center">
 					<div class="col-lg-6 text-center">
@@ -515,8 +547,8 @@ const Product = () => {
 				</div>
 			</div>
 		</div> */}
-	</section>
-        </>
-    )
+			</section>
+		</>
+	)
 }
 export default Product
---
+++ src/redux/store.jsx
@@ -0,0 +1,9 @@
+// src/redux/store.js
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './cartslice';
+
+export const store = configureStore({
+    reducer: {
+        cart: cartReducer,
+    },
+});
